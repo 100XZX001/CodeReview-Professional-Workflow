894587cde633caede0615abd5828a0a4169c3b90
YUTA <yuvraj467229@gmail.com>
2026-04-23 22:28:33 +0530

    Add files via upload

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Criticrl  environment server components."""
+
+from .CriticRL__environment import CriticrlEnvironment
+
+__all__ = ["CriticrlEnvironment"]
